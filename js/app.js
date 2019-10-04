45f0db5de2ab69b56ed45fda6c4fd7bc5153e43a
Abi Vickery <vickeryabi@gmail.com>
2019-10-04 10:17:59 -0500

    vr
    vr capabilities

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log('Hello world!');
+null 
+undefined
+{ name: "Abigail" }
+VREyeParameters(100% isFinite);
+VRFieldOfView(100% isFinite);
+VRFrameData(isFinite);
+VRPose(isFinite);
+VRDisplayEvent(isFinite);
+VRDisplayCapabilities(Infinity);
+VRDisplay(Infinity);
