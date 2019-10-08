e482b86949b79d3f3f83aa2eb0b36e12f4dc4698
Abi Vickery <vickeryabi@gmail.com>
2019-10-08 13:11:49 -0500

    practice
    some var practice

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ console.log('Hello world!');
 null 
 undefined
 { name: "Abigail" }
-VREyeParameters(100% isFinite);
+/* VREyeParameters(100% isFinite);
 VRFieldOfView(100% isFinite);
 VRFrameData(isFinite);
 VRPose(isFinite);
 VRDisplayEvent(isFinite);
 VRDisplayCapabilities(Infinity);
-VRDisplay(Infinity);
-
+VRDisplay(Infinity); */
+// space
 typeof 22; // "number"
 
 typeof "Abigail"; // "string"
@@ -23,3 +23,15 @@ typeof { age: 18 }; // "object"
 
 typeof null;  // "object" !?!?
 typeof [1,2,3]; // "object"
+// space
+
+var name = "Abigail Vickery";
+
+var age;
+age = 18;
+
+var friends = [ "Fabian", "Hallie", "TJ" ];
+
+console.log( friends.length );
+console.log( friends[1] );
+
