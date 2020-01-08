b38d99e7e9e13127267320b56c192e1229c325f1
Abi Vickery <vickeryabi@gmail.com>
2020-01-08 10:40:58 -0600

    uniqSort

--- js/app.js
+++ js/app.js
@@ -725,21 +725,47 @@ console.log(quayHotel.rooms);
 console.log(quayHotel.checkAvailability());
 
 
-isUnique = (arr) => {
+// isUnique = (arr) => {
+//     const breadcrumbs = {};
+//     let result = true;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         console.log(~~~~ LOOP ~~~~ i === ${i}`);
+//         if (breadcrumbs[arr[i]]) {
+//             result = false;
+//         } else {
+//             breadcrumbs[arr[i]] = true;
+//         }
+//     }
+
+//     return result;
+// };
+
+// console.log(isUnique([1,2,3]) === true);
+// // console.log(isUnique([1,1,3]) === false);
+
+
+
+//Task: Transform this simple sorting algorithm into a unique sort.
+// It should not return any duplicate values in the sorted array.
+
+// input: [1,5,2,1] => output: [1,2,5]
+// input: [4,2,2,3,2,2,2] => output: [2,3,4]
+
+/* const uniqSort = function(arr) {
     const breadcrumbs = {};
-    let result = true;
 
-    for (let i = 0; i < arr.length; i++) {
-        console.log(~~~~ LOOP ~~~~ i === ${i}`);
-        if (breadcrumbs[arr[i]]) {
-            result = false;
-        } else {
-            breadcrumbs[arr[i]] = true;
-        }
-    }
-
-    return result;
+    return arr.sort((a, b) => a - b);
 };
 
-console.log(isUnique([1,2,3]) === true);
-// console.log(isUnique([1,1,3]) === false);
+uniqSort([4,2,2,3,2,2,2]); // => [2,3,4]
+*/
+
+const uniqSort = function(arr) {
+    const breadcrumbs = {};
+
+    return arr.sort((a, b) => a - b);
+};
+
+uniqSort([4,2,2,3,2,2,2]); // => [2,3,4]
+console.log(uniqSort);
